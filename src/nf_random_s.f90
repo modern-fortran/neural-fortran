2db890d81668b236835e576e4aa190047a39a7b1
Damian Rouson <rouson@lbl.gov>
2022-04-05 16:13:50 -0700

    refac(random): mv procedure definition to submod

---
+++ src/nf_random_s.f90
@@ -0,0 +1,26 @@
+submodule(mod_random) submod_random
+
+  ! Provides a random number generator with
+  ! normal distribution, centered on zero.
+
+  implicit none
+
+  real(rk), parameter :: pi = 4 * atan(1._rk)
+
+contains
+
+  module procedure randn1d
+    real(rk) :: r2(n)
+    call random_number(r)
+    call random_number(r2)
+    r = sqrt(-2 * log(r)) * cos(2 * pi * r2)
+  end procedure randn1d
+
+  module procedure randn2d
+    real(rk) :: r2(m, n)
+    call random_number(r)
+    call random_number(r2)
+    r = sqrt(-2 * log(r)) * cos(2 * pi * r2)
+  end procedure randn2d
+
+end submodule submod_random
