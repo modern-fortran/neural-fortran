3e90314b98e645dab76aa42b5140be8954d38452
Damian Rouson <rouson@lbl.gov>
2022-04-06 11:13:38 -0700

    doc(src/): convert (almost) all comments to ford
    Comments that appear just after the beginning of a module or
    procedure now start with a double bang (!!) instead of a single
    bang (!).  Comments elsewhere are unchanged.

--- src/mod_activation.f90
+++ src/mod_activation.f90
@@ -1,6 +1,6 @@
 module mod_activation
 
-  ! A collection of activation functions and their derivatives.
+  !! A collection of activation functions and their derivatives.
 
   use mod_kinds, only: ik, rk
 
@@ -26,14 +26,14 @@ module mod_activation
 contains
 
   pure function gaussian(x) result(res)
-    ! Gaussian activation function.
+    !! Gaussian activation function.
     real(rk), intent(in) :: x(:)
     real(rk) :: res(size(x))
     res = exp(-x**2)
   end function gaussian
 
   pure function gaussian_prime(x) result(res)
-    ! First derivative of the Gaussian activation function.
+    !! First derivative of the Gaussian activation function.
     real(rk), intent(in) :: x(:)
     real(rk) :: res(size(x))
     res = -2 * x * gaussian(x)
@@ -47,7 +47,7 @@ contains
   end function relu
 
   pure function relu_prime(x) result(res)
-    ! First derivative of the REctified Linear Unit (RELU) activation function.
+    !! First derivative of the REctified Linear Unit (RELU) activation function.
     real(rk), intent(in) :: x(:)
     real(rk) :: res(size(x))
     where (x > 0)
@@ -58,21 +58,21 @@ contains
   end function relu_prime
 
   pure function sigmoid(x) result(res)
-    ! Sigmoid activation function.
+    !! Sigmoid activation function.
     real(rk), intent(in) :: x(:)
     real(rk) :: res(size(x))
     res = 1 / (1 + exp(-x))
   endfunction sigmoid
 
   pure function sigmoid_prime(x) result(res)
-    ! First derivative of the sigmoid activation function.
+    !! First derivative of the sigmoid activation function.
     real(rk), intent(in) :: x(:)
     real(rk) :: res(size(x))
     res = sigmoid(x) * (1 - sigmoid(x))
   end function sigmoid_prime
 
   pure function step(x) result(res)
-    ! Step activation function.
+    !! Step activation function.
     real(rk), intent(in) :: x(:)
     real(rk) :: res(size(x))
     where (x > 0)
@@ -83,24 +83,24 @@ contains
   end function step
 
   pure function step_prime(x) result(res)
-    ! First derivative of the step activation function.
+    !! First derivative of the step activation function.
     real(rk), intent(in) :: x(:)
     real(rk) :: res(size(x))
     res = 0
   end function step_prime
 
   pure function tanhf(x) result(res)
-    ! Tangent hyperbolic activation function. 
-    ! Same as the intrinsic tanh, but must be 
-    ! defined here so that we can use procedure
-    ! pointer with it.
+    !! Tangent hyperbolic activation function. 
+    !! Same as the intrinsic tanh, but must be 
+    !! defined here so that we can use procedure
+    !! pointer with it.
     real(rk), intent(in) :: x(:)
     real(rk) :: res(size(x))
     res = tanh(x)
   end function tanhf
 
   pure function tanh_prime(x) result(res)
-    ! First derivative of the tanh activation function.
+    !! First derivative of the tanh activation function.
     real(rk), intent(in) :: x(:)
     real(rk) :: res(size(x))
     res = 1 - tanh(x)**2
