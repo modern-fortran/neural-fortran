b1c8f38d7e5c4dc3824f1599a3d2d02d04b930f1
Damian Rouson <rouson@lbl.gov>
2022-04-09 18:30:49 -0700

    refac(mod_io): mv procedure defs to submodule

--- src/mod_io.f90
+++ src/mod_io.f90
@@ -9,35 +9,21 @@ module mod_io
   public :: read_binary_file
 
   interface read_binary_file
-    module procedure :: read_binary_file_1d, read_binary_file_2d
+
+    module subroutine read_binary_file_1d(filename, dtype, nrec, array)
+      implicit none
+      character(len=*), intent(in) :: filename
+      integer(ik), intent(in) :: dtype, nrec
+      real(rk), allocatable, intent(in out) :: array(:)
+    end subroutine read_binary_file_1d
+
+    module subroutine read_binary_file_2d(filename, dtype, dsize, nrec, array)
+      implicit none
+      character(len=*), intent(in) :: filename
+      integer(ik), intent(in) :: dtype, dsize, nrec
+      real(rk), allocatable, intent(in out) :: array(:,:)
+    end subroutine read_binary_file_2d
+
   end interface read_binary_file
-
-contains
-
-  subroutine read_binary_file_1d(filename, dtype, nrec, array)
-    character(len=*), intent(in) :: filename
-    integer(ik), intent(in) :: dtype, nrec
-    real(rk), allocatable, intent(in out) :: array(:)
-    integer(ik) :: fileunit
-    allocate(array(nrec))
-    open(newunit=fileunit, file=filename, access='direct',&
-         action='read', recl=dtype * nrec, status='old')
-    read(fileunit, rec=1) array
-    close(fileunit)
-  end subroutine read_binary_file_1d
-
-  subroutine read_binary_file_2d(filename, dtype, dsize, nrec, array)
-    character(len=*), intent(in) :: filename
-    integer(ik), intent(in) :: dtype, dsize, nrec
-    real(rk), allocatable, intent(in out) :: array(:,:)
-    integer(ik) :: fileunit, i
-    allocate(array(dsize, nrec))
-    open(newunit=fileunit, file=filename, access='direct',&
-         action='read', recl=dtype * dsize, status='old')
-    do i = 1, nrec
-      read(fileunit, rec=i) array(:,i)
-    end do
-    close(fileunit)
-  end subroutine read_binary_file_2d
 
 end module mod_io
