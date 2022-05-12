1192ef04d3d6efe35bc5ed4688a3b6c0c9cbfd03
Milan Curcic <caomaco@gmail.com>
2022-05-12 19:25:47 -0400

    First test for maxpool2d % forward(); does not pass yet

--- test/test_maxpool2d_layer.f90
+++ test/test_maxpool2d_layer.f90
@@ -8,6 +8,8 @@ program test_maxpool2d_layer
 
   type(layer) :: maxpool_layer, input_layer
   integer, parameter :: pool_size = 2, stride = 2
+  integer, parameter :: input_shape(3) = [3, 32, 32]
+  integer, parameter :: output_shape(3) = [3, 16, 16]
   real, allocatable :: sample_input(:,:,:), output(:,:,:)
   logical :: ok = .true.
 
@@ -23,7 +25,7 @@ program test_maxpool2d_layer
     write(stderr, '(a)') 'maxpool2d layer should not be marked as initialized yet.. failed'
   end if
 
-  input_layer = input([3, 32, 32])
+  input_layer = input(input_shape)
   call maxpool_layer % init(input_layer)
 
   if (.not. maxpool_layer % initialized) then
@@ -31,14 +33,30 @@ program test_maxpool2d_layer
     write(stderr, '(a)') 'maxpool2d layer should now be marked as initialized.. failed'
   end if
 
-  if (.not. all(maxpool_layer % input_layer_shape == [3, 32, 32])) then
+  if (.not. all(maxpool_layer % input_layer_shape == input_shape)) then
     ok = .false.
     write(stderr, '(a)') 'maxpool2d layer input layer shape should be correct.. failed'
   end if
 
-  if (.not. all(maxpool_layer % layer_shape == [3, 16, 16])) then
+  if (.not. all(maxpool_layer % layer_shape == output_shape)) then
     ok = .false.
     write(stderr, '(a)') 'maxpool2d layer input layer shape should be correct.. failed'
+  end if
+
+  allocate(sample_input(3, 32, 32))
+  sample_input = 0
+  sample_input(:,2,2) = 1 ! Set lower-right corner pixel of the upper-left pool
+
+  select type(this_layer => input_layer % p); type is(input3d_layer)
+    call this_layer % set(sample_input)
+  end select
+
+  call maxpool_layer % forward(input_layer)
+  call maxpool_layer % get_output(output)
+
+  if (.not. all(output(:,1,1) == [1, 1, 1])) then
+    ok = .false.
+    write(stderr, '(a)') 'maxpool2d layer forward pass correctly propagates the max value.. failed'
   end if
 
   if (ok) then
