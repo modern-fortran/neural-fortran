9f473958905c168767fd53333e55d67f36740c70
milancurcic <caomaco@gmail.com>
2022-05-27 15:08:37 -0400

    Download test data files from repo

--- test/test_io_hdf5.f90
+++ test/test_io_hdf5.f90
@@ -1,12 +1,34 @@
 program test_io_hdf5
 
+  use iso_fortran_env, only: stderr => error_unit
+  use nf_datasets, only: download_and_unpack, keras_model_dense_mnist_url
   use nf_io_hdf5, only: get_h5_attribute_string
 
   implicit none
 
   character(:), allocatable :: attr
-  character(*), parameter :: test_data_path = 'test/data/mnist_dense.h5'
+  character(*), parameter :: test_data_path = 'keras_dense_mnist.h5'
+  logical :: file_exists
+  logical :: ok
+
+  print *, keras_model_dense_mnist_url
+
+  inquire(file=test_data_path, exist=file_exists)
+  if (.not. file_exists) call download_and_unpack(keras_model_dense_mnist_url)
 
   attr = get_h5_attribute_string(test_data_path, '.', 'backend')
+
+  if (.not. attr == 'tensorflow') then
+    ok = .false.
+    write(stderr, '(a)') &
+      'HDF5 variable length string attribute not read correctly.. failed'
+  end if
+
+  if (ok) then
+    print '(a)', 'test_io_hdf5: All tests passed.'
+  else
+    write(stderr, '(a)') 'test_io_hdf5: One or more tests failed.'
+    stop 1
+  end if
 
 end program test_io_hdf5
--- test/test_parse_keras_model.f90
+++ test/test_parse_keras_model.f90
@@ -1,17 +1,24 @@
 program test_parse_keras_model
 
+  use iso_fortran_env, only: stderr => error_unit
+  use nf_datasets, only: download_and_unpack, keras_model_dense_mnist_url
   use nf_io_hdf5, only: get_h5_attribute_string
   use json_module
 
   implicit none
 
   character(:), allocatable :: model_config_string
-  character(*), parameter :: test_data_path = 'test/data/mnist_dense.h5'
+  character(*), parameter :: test_data_path = 'keras_dense_mnist.h5'
   type(json_core) :: json
   type(json_value), pointer :: model_config, layers, next_layer, layer
   character(:), allocatable :: class_name, layer_type
   logical :: found
   integer :: n, num_layers
+  logical :: file_exists
+  logical :: ok
+
+  inquire(file=test_data_path, exist=file_exists)
+  if (.not. file_exists) call download_and_unpack(keras_model_dense_mnist_url)
 
   model_config_string = &
     get_h5_attribute_string(test_data_path, '.', 'model_config')
@@ -20,14 +27,25 @@ program test_parse_keras_model
   call json % get(model_config, 'config.layers', layers)
 
   num_layers = json % count(layers)
-  print *, 'This model has', num_layers, 'layers.'
 
   do n = 1, num_layers
     call json % get_child(layers, n, layer)
-    print *, 'Layer', n
+    !print *, 'Layer', n
     !call json % print(layer)
     call json % get(layer, 'class_name', layer_type)
-    print *, layer_type
+    !print *, layer_type
   end do
+
+  if (.not. num_layers == 3) then
+    ok = .false.
+    write(stderr, '(a)') 'Keras dense MNIST model has 3 layers.. failed'
+  end if
+
+  if (ok) then
+    print '(a)', 'test_parse_keras_model: All tests passed.'
+  else
+    write(stderr, '(a)') 'test_parse_keras_model: One or more tests failed.'
+    stop 1
+  end if
 
 end program test_parse_keras_model
