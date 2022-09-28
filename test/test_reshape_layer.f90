24eec643f4f934cf843bc32cc81f264b02caebd3
milancurcic <caomaco@gmail.com>
2022-09-28 13:56:37 -0400

    Enable reading reshape layers from Keras h5

--- test/test_reshape_layer.f90
+++ test/test_reshape_layer.f90
@@ -2,6 +2,7 @@ program test_reshape_layer
 
   use iso_fortran_env, only: stderr => error_unit
   use nf, only: input, network, reshape_layer => reshape
+  use nf_datasets, only: download_and_unpack, keras_reshape_url
 
   implicit none
 
@@ -9,6 +10,8 @@ program test_reshape_layer
   real, allocatable :: sample_input(:), output(:,:,:)
   integer, parameter :: output_shape(3) = [3, 32, 32]
   integer, parameter :: input_size = product(output_shape)
+  character(*), parameter :: keras_reshape_path = 'keras_reshape.h5'
+  logical :: file_exists
   logical :: ok = .true.
 
   ! Create the network
@@ -37,6 +40,31 @@ program test_reshape_layer
 
   if (.not. all(reshape(sample_input, output_shape) == output)) then
     write(stderr, '(a)') 'the reshape layer produces expected output values.. failed'
+    ok = .false.
+  end if
+
+  ! Now test reading the reshape layer from a Keras h5 model.
+  inquire(file=keras_reshape_path, exist=file_exists)
+  if (.not. file_exists) call download_and_unpack(keras_reshape_url)
+
+  net = network(keras_reshape_path)
+
+  if (.not. size(net % layers) == 2) then
+    write(stderr, '(a)') 'the reshape network from Keras has the correct size.. failed'
+    ok = .false.
+  end if
+
+  if (.not. net % layers(2) % name == 'reshape') then
+    write(stderr, '(a)') 'the 2nd layer of the reshape network from Keras is a reshape layer.. failed'
+    ok = .false.
+  end if
+
+  ! Test that the output shape checks out
+  call net % layers(1) % get_output(sample_input)
+  call net % layers(2) % get_output(output)
+
+  if (.not. all(shape(output) == [1, 28, 28])) then
+    write(stderr, '(a)') 'the target shape of the reshape layer is correct.. failed'
     ok = .false.
   end if
 
