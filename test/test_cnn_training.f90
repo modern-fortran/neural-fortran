7500f13a30412f4825af34beec03ae15109eca5b
milancurcic <caomaco@gmail.com>
2022-10-11 13:37:15 -0400

    Begin work on CNN training test

---
+++ test/test_cnn_training.f90
@@ -0,0 +1,85 @@
+program test_cnn_training
+
+  use iso_fortran_env, only: stderr => error_unit
+  use nf, only: network, sgd, &
+    input, conv2d, maxpool2d, flatten, dense, reshape, &
+    load_mnist, label_digits
+
+  implicit none
+
+  type(network) :: net
+
+  real, allocatable :: training_images(:,:), training_labels(:)
+  real, allocatable :: validation_images(:,:), validation_labels(:)
+  real, allocatable :: testing_images(:,:), testing_labels(:)
+  real, allocatable :: input_reshaped(:,:,:,:)
+  real :: acc
+  logical :: ok
+  integer :: n
+  integer, parameter :: num_epochs = 10
+
+  call load_mnist(training_images, training_labels, &
+                  validation_images, validation_labels, &
+                  testing_images, testing_labels)
+
+  !training_data = reshape(training_images(:,:), shape=[1,28,28,50000])
+  
+  net = network([ &
+    input(768), &
+    reshape([1,28,28]), &
+    conv2d(filters=8, kernel_size=3, activation='relu'), &
+    maxpool2d(pool_size=2), &
+    conv2d(filters=16, kernel_size=3, activation='relu'), &
+    maxpool2d(pool_size=2), &
+    flatten(), &
+    dense(10, activation='softmax') &
+  ])
+
+  epochs: do n = 1, num_epochs
+
+    call net % train( &
+      training_images, &
+      label_digits(training_labels), &
+      batch_size=100, &
+      epochs=1, &
+      optimizer=sgd(learning_rate=3.) &
+    )
+
+    if (this_image() == 1) &
+      print '(a,i2,a,f5.2,a)', 'Epoch ', n, ' done, Accuracy: ', accuracy( &
+        net, validation_images, label_digits(validation_labels)) * 100, ' %'
+
+  end do epochs
+
+  !acc = accuracy(net, input_reshaped, label_digits(testing_labels(:1000)))
+
+  !if (acc < 0.97) then
+  !  write(stderr, '(a)') &
+  !    'Pre-trained network accuracy should be > 0.97.. failed'
+  !  ok = .false.
+  !end if
+
+  if (ok) then
+    print '(a)', 'test_cnn_from_keras: All tests passed.'
+  else
+    write(stderr, '(a)') &
+      'test_cnn_from_keras: One or more tests failed.'
+    stop 1
+  end if
+
+contains
+
+  real function accuracy(net, x, y)
+    type(network), intent(in out) :: net
+    real, intent(in) :: x(:,:), y(:,:)
+    integer :: i, good
+    good = 0
+    do i = 1, size(x, dim=2)
+      if (all(maxloc(net % predict(x(:,i))) == maxloc(y(:,i)))) then
+        good = good + 1
+      end if
+    end do
+    accuracy = real(good) / size(x, dim=2)
+  end function accuracy
+
+end program test_cnn_training
