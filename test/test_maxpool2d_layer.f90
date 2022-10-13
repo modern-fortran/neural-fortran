7076e41e581123d6d9c35dae8181b6510d54ed4f
milancurcic <caomaco@gmail.com>
2022-10-13 16:25:34 -0400

    Add test for maxpool2d backward pass

--- test/test_maxpool2d_layer.f90
+++ test/test_maxpool2d_layer.f90
@@ -3,6 +3,7 @@ program test_maxpool2d_layer
   use iso_fortran_env, only: stderr => error_unit
   use nf, only: maxpool2d, input, layer
   use nf_input3d_layer, only: input3d_layer
+  use nf_maxpool2d_layer, only: maxpool2d_layer
 
   implicit none
 
@@ -11,9 +12,9 @@ program test_maxpool2d_layer
   integer, parameter :: channels = 3, width = 32
   integer, parameter :: input_shape(3) = [channels, width, width]
   integer, parameter :: output_shape(3) = [channels, width / 2, width / 2]
-  real, allocatable :: sample_input(:,:,:), output(:,:,:)
+  real, allocatable :: sample_input(:,:,:), output(:,:,:), gradient(:,:,:)
   integer :: i, j
-  logical :: ok = .true.
+  logical :: ok = .true., gradient_ok = .true.
 
   maxpool_layer = maxpool2d(pool_size)
 
@@ -45,8 +46,8 @@ program test_maxpool2d_layer
     write(stderr, '(a)') 'maxpool2d layer input layer shape should be correct.. failed'
   end if
 
+  ! Allocate and initialize sample input data
   allocate(sample_input(channels, width, width))
-
   do concurrent(i = 1:width, j = 1:width)
     sample_input(:,i,j) = i * j
   end do
@@ -67,6 +68,30 @@ program test_maxpool2d_layer
       end if
     end do
   end do
+
+  ! Test the backward pass
+  ! Allocate and initialize the downstream gradient field
+  allocate(gradient, source=output)
+
+  ! Make a backward pass
+  call maxpool_layer % backward(input_layer, gradient)
+
+  select type(this_layer => maxpool_layer % p); type is(maxpool2d_layer)
+    do j = 1, width
+      do i = 1, width
+        if (mod(i,2) == 0 .and. mod(j,2) == 0) then
+          if (.not. all(sample_input(:,i,j) == this_layer % gradient(:,i,j))) gradient_ok = .false.
+        else
+          if (.not. all(this_layer % gradient(:,i,j) == 0)) gradient_ok = .false.
+        end if
+      end do
+    end do
+  end select
+
+  if (.not. gradient_ok) then
+    ok = .false.
+    write(stderr, '(a)') 'maxpool2d layer backward pass produces the correct dL/dx.. failed'
+  end if
 
   if (ok) then
     print '(a)', 'test_maxpool2d_layer: All tests passed.'
