996b42a4ac491210e4d9a19739f31a48bfa5e8af
milancurcic <caomaco@gmail.com>
2022-12-21 13:40:01 -0500

    Test getting and setting parameters

--- test/test_get_set_network_params.f90
+++ test/test_get_set_network_params.f90
@@ -5,7 +5,10 @@ program test_get_set_network_params
   type(network) :: net
   integer :: n
   logical :: ok = .true.
+  real :: test_params_dense(8) = [1, 2, 3, 4, 5, 6, 7, 8]
+  real :: test_params_conv2d(10) = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+  ! First test get_num_params()
   net = network([ &
     input([3, 5, 5]), & ! 5 x 5 image with 3 channels
     conv2d(filters=2, kernel_size=3), & ! kernel shape [2, 3, 3, 3], output shape [2, 3, 3], 56 parameters total
@@ -21,6 +24,42 @@ program test_get_set_network_params
   if (.not. all(net % layers % get_num_params() == [0, 56, 0, 76])) then
     ok = .false.
     write(stderr, '(a)') 'Sizes of layer parameters are all as expected.. failed'
+  end if
+
+  ! Next, test set_params() and get_params() for a dense layer
+  net = network([ &
+    input(3), &
+    dense(2) &
+  ])
+
+  call net % set_params(test_params_dense)
+
+  if (.not. all(net % get_params() == test_params_dense)) then
+    ok = .false.
+    write(stderr, '(a)') 'Dense network params match the params that we set to it.. failed'
+  end if
+
+  if (.not. all(net % get_params() == net % layers(2) % get_params())) then
+    ok = .false.
+    write(stderr, '(a)') 'Single dense layer network params match that layer''s params.. failed'
+  end if
+
+  ! Finally, test set_params() and get_params() for a conv2d layer
+  net = network([ &
+    input([1, 3, 3]), &
+    conv2d(filters=1, kernel_size=3) &
+  ])
+
+  call net % set_params(test_params_conv2d)
+
+  if (.not. all(net % get_params() == test_params_conv2d)) then
+    ok = .false.
+    write(stderr, '(a)') 'Conv network params match the params that we set to it.. failed'
+  end if
+
+  if (.not. all(net % get_params() == net % layers(2) % get_params())) then
+    ok = .false.
+    write(stderr, '(a)') 'Single conv2d layer network params match that layer''s params.. failed'
   end if
 
   if (ok) then
