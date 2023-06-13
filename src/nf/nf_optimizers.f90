677dc5e6e437bedb48bad352292d3e3829982802
milancurcic <caomaco@gmail.com>
2023-06-13 12:48:49 -0400

    Defining the SGD minimization step in the optimizer type

--- src/nf/nf_optimizers.f90
+++ src/nf/nf_optimizers.f90
@@ -9,13 +9,35 @@ module nf_optimizers
 
   type, abstract :: optimizer_base_type
     character(:), allocatable :: name
+  contains
+    procedure(minimize_interface), deferred :: minimize
   end type optimizer_base_type
+
+  abstract interface
+    elemental subroutine minimize_interface(self, weight, gradient)
+      import :: optimizer_base_type
+      class(optimizer_base_type), intent(in) :: self
+      real, intent(inout) :: weight
+      real, intent(in) :: gradient
+    end subroutine minimize_interface
+  end interface
 
   type, extends(optimizer_base_type) :: sgd
     !! Stochastic Gradient Descent optimizer
     real :: learning_rate
     real :: momentum = 0 !TODO
     logical :: nesterov = .false. !TODO
+  contains
+    procedure :: minimize => minimize_sgd
   end type sgd
+
+contains
+
+  elemental subroutine minimize_sgd(self, weight, gradient)
+    class(sgd), intent(in) :: self
+    real, intent(inout) :: weight
+    real, intent(in) :: gradient
+    weight = weight - self % learning_rate * gradient
+  end subroutine minimize_sgd
 
 end module nf_optimizers
