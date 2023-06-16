6efb8c449038cf53728517b26f9495f52b6fe18f
Spnetic-5 <sspowar_b19@el.vjti.ac.in>
2023-06-16 10:10:31 -0400

    Added RMSProp subroutine

--- example/quadratic.f90
+++ example/quadratic.f90
@@ -3,9 +3,10 @@ program quadratic_fit
   ! stochastic gradient descent, batch gradient descent, and minibatch gradient
   ! descent.
   use nf, only: dense, input, network
+  use nf_dense_layer, only: dense_layer
 
   implicit none
-  type(network) :: net_sgd, net_batch_sgd, net_minibatch_sgd
+  type(network) :: net_sgd, net_batch_sgd, net_minibatch_sgd, net_rms_prop
 
   ! Training parameters
   integer, parameter :: num_epochs = 1000
@@ -13,11 +14,12 @@ program quadratic_fit
   integer, parameter :: test_size = 30
   integer, parameter :: batch_size = 10
   real, parameter :: learning_rate = 0.01
+  real, parameter :: decay_rate = 0.9
 
   ! Input and output data
   real, allocatable :: x(:), y(:) ! training data
   real, allocatable :: xtest(:), ytest(:) ! testing data
-  real, allocatable :: ypred_sgd(:), ypred_batch_sgd(:), ypred_minibatch_sgd(:)
+  real, allocatable :: ypred_sgd(:), ypred_batch_sgd(:), ypred_minibatch_sgd(:), ypred_rms_prop(:)
 
   integer :: i, n
 
@@ -42,6 +44,7 @@ program quadratic_fit
   net_sgd = network([input(1), dense(3), dense(1)])
   net_batch_sgd = network([input(1), dense(3), dense(1)])
   net_minibatch_sgd = network([input(1), dense(3), dense(1)])
+  net_rms_prop = network([input(1), dense(3), dense(1)])
 
   ! Print network info to stdout; this will be the same for all three networks.
   call net_sgd % print_info()
@@ -55,15 +58,20 @@ program quadratic_fit
   ! Mini-batch SGD optimizer
   call minibatch_gd_optimizer(net_minibatch_sgd, x, y, learning_rate, num_epochs, batch_size)
 
+  ! RMSProp optimizer
+  call rmsprop_optimizer(net_rms_prop, x, y, learning_rate, num_epochs, decay_rate)
+
   ! Calculate predictions on the test set
   ypred_sgd = [(net_sgd % predict([xtest(i)]), i = 1, test_size)]
   ypred_batch_sgd = [(net_batch_sgd % predict([xtest(i)]), i = 1, test_size)]
   ypred_minibatch_sgd = [(net_minibatch_sgd % predict([xtest(i)]), i = 1, test_size)]
+  ypred_rms_prop = [(net_rms_prop % predict([xtest(i)]), i = 1, test_size)]
 
   ! Print the mean squared error
-  print '("Stochastic gradient descent MSE:", f9.6)', sum((ypred_sgd - ytest)**2) / size(ytest)
+  print '(" Stochastic gradient descent MSE:", f9.6)', sum((ypred_sgd - ytest)**2) / size(ytest)
   print '("     Batch gradient descent MSE: ", f9.6)', sum((ypred_batch_sgd - ytest)**2) / size(ytest)
   print '(" Minibatch gradient descent MSE: ", f9.6)', sum((ypred_minibatch_sgd - ytest)**2) / size(ytest)
+  print '("                    RMSProp MSE: ", f9.6)', sum((ypred_rms_prop - ytest)**2) / size(ytest)
 
 contains
 
@@ -160,6 +168,49 @@ contains
       end do
     end do
   end subroutine minibatch_gd_optimizer
+
+  subroutine rmsprop_optimizer(net, x, y, learning_rate, num_epochs, decay_rate)
+    ! RMSprop optimizer for updating weights using root mean square
+    type(network), intent(inout) :: net
+    real, intent(in) :: x(:), y(:)
+    real, intent(in) :: learning_rate, decay_rate
+    integer, intent(in) :: num_epochs
+    integer :: i, j, n
+    real :: epsilon
+    real, allocatable :: rms_weights(:,:), rms_gradients(:,:)
+    real, allocatable :: weights(:,:), gradients(:,:) 
+
+    print *, "Running RMSprop optimizer..."
+
+    ! Initialize rms_weights and rms_gradients arrays
+    allocate(rms_weights(3,1), rms_gradients(3,1))
+
+    rms_weights = 0.0
+    rms_gradients = 0.0
+    epsilon = 1e-8  ! Small constant to avoid division by zero
+
+    do n = 1, num_epochs
+      do i = 1, size(x)
+        call net % forward([x(i)])
+        call net % backward([y(i)])
+
+        ! Update rms_weights and rms_gradients
+        do j = 1, size(net % layers)
+          select type (this_layer => net % layers(j) % p)
+          type is (dense_layer)   
+            weights = this_layer % weights
+            gradients = this_layer % dw
+            rms_weights = decay_rate * rms_weights + (1.0 - decay_rate) * (weights*weights)
+            rms_gradients = decay_rate * rms_gradients + (1.0 - decay_rate) * (gradients*gradients)
+            ! Update weights using RMSprop update rule
+            weights = weights - (learning_rate / sqrt(rms_weights + epsilon)) * gradients
+          end select
+        end do
+      end do
+    end do
+
+  end subroutine rmsprop_optimizer
+
 
   subroutine shuffle(arr)
     ! Shuffle an array using the Fisher-Yates algorithm.
