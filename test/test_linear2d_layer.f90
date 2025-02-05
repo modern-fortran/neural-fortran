f28ecc0ffc5fcec01ba2419e738736f9ff0cd5ef
Mikhail Voronov <mikivo@list.ru>
2025-02-05 12:00:24 +0400

    update linear2d_layer tests for batch last

--- test/test_linear2d_layer.f90
+++ test/test_linear2d_layer.f90
@@ -4,14 +4,14 @@ program test_linear2d_layer
   implicit none
 
   logical :: ok = .true.
-  real :: sample_input(2, 3, 4) = reshape(&
-      [0.1, 0.2, 0.1, 0.2, 0.1, 0.2, 0.1, 0.2, 0.1, 0.2, 0.1, 0.2,&
-       0.1, 0.2, 0.1, 0.2, 0.1, 0.2, 0.1, 0.2, 0.1, 0.2, 0.1, 0.2],&
-      [2, 3, 4]) ! first batch are 0.1, second 0.2
-  real :: sample_gradient(2, 3, 1) = reshape([2., 3., 2., 3., 2., 3.], [2, 3, 1])
+  real :: sample_input(3, 4, 2) = reshape(&
+      [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,&
+       0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2],&
+      [3, 4, 2]) ! first batch are 0.1, second 0.2
+  real :: sample_gradient(3, 1, 2) = reshape([2., 2., 2., 3., 3., 3.], [3, 1, 2])
   type(linear2d_layer) :: linear
 
-  linear = linear2d_layer(batch_size=2, sequence_length=3, in_features=4, out_features=1)
+  linear = linear2d_layer(sequence_length=3, in_features=4, out_features=1, batch_size=2)
   call linear % init([4])
 
   call test_linear2d_layer_forward(linear, ok, sample_input)
@@ -21,11 +21,11 @@ contains
   subroutine test_linear2d_layer_forward(linear, ok, input)
     type(linear2d_layer), intent(in out) :: linear
     logical, intent(in out) :: ok
-    real, intent(in) :: input(2, 3, 4)
+    real, intent(in) :: input(3, 4, 2)
     real :: output_shape(3)
     real :: output_flat(6)
-    real :: expected_shape(3) = [2, 3, 1]
-    real :: expected_output_flat(6) = [0.15, 0.19, 0.15, 0.19, 0.15, 0.19]
+    real :: expected_shape(3) = [3, 1, 2]
+    real :: expected_output_flat(6) = [0.15, 0.15, 0.15, 0.19, 0.19, 0.19]
 
     call linear % forward(input)
 
@@ -44,23 +44,23 @@ contains
   subroutine test_linear2d_layer_backward(linear, ok, input, gradient)
     type(linear2d_layer), intent(in out) :: linear
     logical, intent(in out) :: ok
-    real, intent(in) :: input(2, 3, 4)
-    real, intent(in) :: gradient(2, 3, 1)
+    real, intent(in) :: input(3, 4, 2)
+    real, intent(in) :: gradient(3, 1, 2)
     real :: gradient_shape(3)
     real :: dw_shape(2)
     real :: db_shape(1)
     real :: gradient_flat(24)
     real :: dw_flat(4)
-    real :: expected_gradient_shape(3) = [2, 3, 4]
+    real :: expected_gradient_shape(3) = [3, 4, 2]
     real :: expected_dw_shape(2) = [4, 1]
     real :: expected_db_shape(1) = [1]
     real :: expected_gradient_flat(24) = [&
-        0.200000003, 0.300000012, 0.200000003, 0.300000012,&
-        0.200000003, 0.300000012, 0.200000003, 0.300000012,&
-        0.200000003, 0.300000012, 0.200000003, 0.300000012,&
-        0.200000003, 0.300000012, 0.200000003, 0.300000012,&
-        0.200000003, 0.300000012, 0.200000003, 0.300000012,&
-        0.200000003, 0.300000012, 0.200000003, 0.300000012&
+        0.200000003, 0.200000003, 0.200000003, 0.200000003,&
+        0.200000003, 0.200000003, 0.200000003, 0.200000003,&
+        0.200000003, 0.200000003, 0.200000003, 0.200000003,&
+        0.300000012, 0.300000012, 0.300000012, 0.300000012,&
+        0.300000012, 0.300000012, 0.300000012, 0.300000012,&
+        0.300000012, 0.300000012, 0.300000012, 0.300000012&
     ]
     real :: expected_dw_flat(4)
     real :: expected_db(1) = [15.0]
