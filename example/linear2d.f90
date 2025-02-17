bbfaf3cda6b0d65d07612f89c65140f1c2d50a9e
Mikhail Voronov <mikivo@list.ru>
2025-02-17 13:10:50 +0400

    linear2d_layer: make example converge

--- example/linear2d.f90
+++ example/linear2d.f90
@@ -5,10 +5,10 @@ program linear2d_example
 
   type(network) :: net
   real :: x(3, 4) = reshape( &
-    [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2], &
+    [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.11, 0.12, 0.13], &
     [3, 4])
-  real :: y(3) = [0.12, 0.1, 0.3]
-  integer, parameter :: num_iterations = 5
+  real :: y(3) = [0.12, 0.1, 0.2]
+  integer, parameter :: num_iterations = 9
   integer :: n
   
   net = network([ &
@@ -22,7 +22,7 @@ program linear2d_example
   do n = 1, num_iterations
     call net % forward(x)
     call net % backward(y, mse())
-    call net % update(optimizer=sgd(learning_rate=1.))
+    call net % update(optimizer=sgd(learning_rate=0.01))
     print '(i4,3(3x,f8.6))', n, net % predict(x)
   end do
 
