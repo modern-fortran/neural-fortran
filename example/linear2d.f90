bfc69d5d77984ccdcbcd7d9a42582bdc2ee6ab32
Mikhail Voronov <mikivo@list.ru>
2025-02-17 13:48:36 +0400

    linear2d_layer: add loss stopping and more iterations

--- example/linear2d.f90
+++ example/linear2d.f90
@@ -4,11 +4,14 @@ program linear2d_example
   implicit none
 
   type(network) :: net
+  type(mse) :: loss
   real :: x(3, 4) = reshape( &
     [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.11, 0.12, 0.13], &
     [3, 4])
   real :: y(3) = [0.12, 0.1, 0.2]
-  integer, parameter :: num_iterations = 9
+  real :: preds(3)
+  real :: loss_value
+  integer, parameter :: num_iterations = 500
   integer :: n
   
   net = network([ &
@@ -18,12 +21,19 @@ program linear2d_example
   ])
   
   call net % print_info()
+  loss = mse()
 
   do n = 1, num_iterations
     call net % forward(x)
-    call net % backward(y, mse())
+    call net % backward(y, loss)
     call net % update(optimizer=sgd(learning_rate=0.01))
-    print '(i4,3(3x,f8.6))', n, net % predict(x)
+    preds = net % predict(x)
+    print '(i4,3(3x,f8.6))', n, preds
+    loss_value = loss % eval (y, preds)
+    if (loss_value < 0.01) then
+      print *, 'Loss: ', loss_value
+      return
+    end if
   end do
 
 end program linear2d_example
