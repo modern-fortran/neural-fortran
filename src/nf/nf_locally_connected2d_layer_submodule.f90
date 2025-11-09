b44a151259759dc89ad046e90d2851ccdedb7c34
Jeremie Vandenplas <jeremie.vandenplas@gmail.com>
2025-11-09 18:29:39 +0100

    Support of the argument stride for locally_connected2d_layer

--- src/nf/nf_locally_connected2d_layer_submodule.f90
+++ src/nf/nf_locally_connected2d_layer_submodule.f90
@@ -7,16 +7,17 @@ submodule(nf_locally_connected2d_layer) nf_locally_connected2d_layer_submodule
 
 contains
 
-  module function locally_connected2d_layer_cons(filters, kernel_size, activation) result(res)
-    implicit none
+  module function locally_connected2d_layer_cons(filters, kernel_size, activation, stride) result(res)
     integer, intent(in) :: filters
     integer, intent(in) :: kernel_size
     class(activation_function), intent(in) :: activation
+    integer, intent(in) :: stride
     type(locally_connected2d_layer) :: res
 
     res % kernel_size = kernel_size
     res % filters = filters
     res % activation_name = activation % get_name()
+    res % stride = stride
     allocate(res % activation, source = activation)
   end function locally_connected2d_layer_cons
 
@@ -26,8 +27,11 @@ contains
     integer, intent(in) :: input_shape(:)
 
     self % channels = input_shape(1)
-    self % width = input_shape(2) - self % kernel_size + 1
+    self % width = (input_shape(2) - self % kernel_size) / self % stride +1
 
+    if (mod(input_shape(2) - self % kernel_size , self % stride) /= 0) self % width = self % width + 1
+
+    ! Output of shape: filters x width
     allocate(self % output(self % filters, self % width))
     self % output = 0
 
@@ -63,10 +67,10 @@ contains
     input_width    = size(input, dim=2)
 
     do j = 1, self % width
-      iws = j
-      iwe = j + self % kernel_size - 1
+      iws = self % stride * (j-1) + 1
+      iwe = min(iws + self % kernel_size - 1, input_width)
       do n = 1, self % filters
-        self % z(n, j) = sum(self % kernel(n, j, :, :) * input(:, iws:iwe)) + self % biases(n, j)
+        self % z(n, j) = sum(self % kernel(n, j, :, 1:iwe-iws+1) * input(:, iws:iwe)) + self % biases(n, j)
       end do
     end do
     self % output = self % activation % eval(self % z)
@@ -77,7 +81,7 @@ contains
     class(locally_connected2d_layer), intent(in out) :: self
     real, intent(in) :: input(:,:)
     real, intent(in) :: gradient(:,:)
-    integer :: input_channels, input_width, output_width
+    integer :: input_channels, input_width
     integer :: j, n, k
     integer :: iws, iwe
     real :: gdz(self % filters, self % width)
@@ -86,14 +90,13 @@ contains
 
     input_channels = size(input, dim=1)
     input_width    = size(input, dim=2)
-    output_width   = self % width
 
-    do j = 1, output_width
+    do j = 1, self % width
        gdz(:, j) = gradient(:, j) * self % activation % eval_prime(self % z(:, j))
     end do
 
     do n = 1, self % filters
-       do j = 1, output_width
+       do j = 1, self % width
           db_local(n, j) = gdz(n, j)
        end do
     end do
@@ -102,12 +105,12 @@ contains
     self % gradient = 0.0
 
     do n = 1, self % filters
-       do j = 1, output_width
-          iws = j
-          iwe = j + self % kernel_size - 1
+       do j = 1, self % width
+          iws = self % stride * (j-1) + 1
+          iwe = min(iws + self % kernel_size - 1, input_width)
           do k = 1, self % channels
-             dw_local(n, j, k, :) = dw_local(n, j, k, :) + input(k, iws:iwe) * gdz(n, j)
-             self % gradient(k, iws:iwe) = self % gradient(k, iws:iwe) + self % kernel(n, j, k, :) * gdz(n, j)
+             dw_local(n, j, k, 1:iwe-iws+1) = dw_local(n, j, k, 1:iwe-iws+1) + input(k, iws:iwe) * gdz(n, j)
+             self % gradient(k, iws:iwe) = self % gradient(k, iws:iwe) + self % kernel(n, j, k, 1:iwe-iws+1) * gdz(n, j)
           end do
        end do
     end do
@@ -143,6 +146,5 @@ contains
     dw_ptr(1:size(self % dw)) => self % dw
     db_ptr(1:size(self % db)) => self % db
   end subroutine get_gradients_ptr
-
 
 end submodule nf_locally_connected2d_layer_submodule
